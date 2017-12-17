1f76555bd96a5d893b04522374ec5e63892dad29
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-17 20:35:06 +0530

    2 way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created';
-  serverName: string = '';
+  serverName: string = 'Server';
 
   constructor() {
     setTimeout(() => {
